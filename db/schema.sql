8985bfd8257890557874e61d63e788c41721ff2e
imanid-code <imanidillahunt@gmail.com>
2021-03-02 07:48:25 -0500

    connection and orm files created

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+drop database if exists burger_db;
+create database burgers_db;
+
+USE burgers_db; 
+
+create table burgers (
+id integer auto_increment,
+burger_name varchar (50),
+devoured boolean,
+primary key (id)
+)
---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name) VALUES ("Salmon Burger");
+INSERT INTO burgers (burger_name) VALUES ("Plant-Based Burger");
+INSERT INTO burgers (burger_name) VALUES ("Bean Burger");
+
+SELECT * FROM burgers; 
+
